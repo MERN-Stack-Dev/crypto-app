a390517399686d1d0862bdff7289a71141c38afd
dhanur350 <dhanur297@gmail.com>
2024-07-21 13:36:42 +0530

    stock chart

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Home, HomeContainer, Login } from './containers';
 
 function App() {
+  
   return (
     <Routes>
       <Route path="/" element={<Login />} />
